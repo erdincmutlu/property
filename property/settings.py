ba4efc1db68995003e6490bbfc842d89037c8859
Erdinc Mutlu <erdincmutlu@gmail.com>
2024-08-01 21:36:06 +0100

    Django postgres connection setup

--- property/settings.py
+++ property/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = "property.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "property",
+        "USER": "postgres",
+        "PASSWORD": "test",
+        "HOST": "localhost",
     }
 }
 
