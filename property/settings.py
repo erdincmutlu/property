08fdf38114d882733b214d096a77913d13523867
Erdinc Mutlu <erdincmutlu@gmail.com>
2024-08-03 23:20:05 +0100

    Message alerts

--- property/settings.py
+++ property/settings.py
@@ -136,3 +136,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Media folder settings
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
+
+# MESSAGES
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
