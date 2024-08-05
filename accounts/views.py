1d60a13d3fd106493da135c40287d246d254b315
Erdinc Mutlu <erdincmutlu@gmail.com>
2024-08-05 13:32:04 +0100

    Dashboard functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
+from contacts.models import Contact
 
 
 def register(request):
@@ -70,9 +71,14 @@ def login(request):
 def logout(request):
     if request.method == "POST":
         auth.logout(request)
-        messages.success(request, 'You are now logged out')
+        messages.success(request, "You are now logged out")
         return redirect("index")
 
 
 def dashboard(request):
-    return render(request, "accounts/dashboard.html")
+    user_contacts = Contact.objects.order_by("-contact_date").filter(
+        user_id=request.user.id
+    )
+
+    context = {"contacts": user_contacts}
+    return render(request, "accounts/dashboard.html", context)
